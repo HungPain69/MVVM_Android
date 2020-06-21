5683d7a49061aa4e982b4d2dd74836c782d57954
HungPain69 <hungpain69@gmail.com>
2020-06-21 14:08:32 +0700

    add LiveData to the ViewModel

--- app/src/main/java/com/example/mvvmandroid/ui/main/MainViewModel.java
+++ app/src/main/java/com/example/mvvmandroid/ui/main/MainViewModel.java
@@ -1,17 +1,21 @@
 package com.example.mvvmandroid.ui.main;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
     // TODO: Implement the ViewModel
     private static final Float usd_to_eu_rate = 0.74F;
     private String dollarText = "";
-    private Float result = 0F;
+    
+    private MutableLiveData<Float> result = new MutableLiveData<>();
+    //Now that the result variable is contained in a mutable LiveData instance
+
     public void setAmount(String value) {
         this.dollarText = value;
-        result = Float.valueOf(dollarText)*usd_to_eu_rate;
+        result.setValue(Float.valueOf(dollarText)*usd_to_eu_rate);
     }
-    public Float getResult()
+    public MutableLiveData<Float> getResult()
     {
         return result;
     }
